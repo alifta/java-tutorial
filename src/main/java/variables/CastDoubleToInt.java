c3d6060272a091fafb3574367c73e2b5f4ef152f
Ali Farrokhtala <farrokhtala@gmail.com>
2023-06-28 14:08:00 -0700

    Delete src/main/java/variables directory

--- src/main/java/variables/CastDoubleToInt.java
+++
@@ -1,9 +0,0 @@
-package variables;
-
-public class CastDoubleToInt {
-    public static void main(String[] args) {
-        double number = 42.1;
-        int doubleNumber = (int) number;
-        System.out.println("Casted nr: " + doubleNumber);
-    }
-}
--- src/main/java/variables/FindLengthStr.java
+++
@@ -1,9 +0,0 @@
-package variables;
-
-public class FindLengthStr {
-    public static void main(String[] args) {
-        String text = "Java Programming";
-        int length = text.length();
-        System.out.println("Length: " + length);
-    }
-}
